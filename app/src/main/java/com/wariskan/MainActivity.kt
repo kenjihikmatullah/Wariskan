39d243bd41b68002e24f8b8de4fb0d29ed34c5c4
Kenji Hikmatullah <iniemailkenji@gmail.com>
2020-02-12 06:47:34 +0700

    - add simple launch screen

--- app/src/main/java/com/wariskan/MainActivity.kt
+++ app/src/main/java/com/wariskan/MainActivity.kt
@@ -3,8 +3,8 @@ package com.wariskan
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil.setContentView
-import com.google.android.gms.ads.MobileAds
 import com.wariskan.R.layout.activity_main
+import com.wariskan.R.style.App
 import com.wariskan.databinding.ActivityMainBinding as Binding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(App)
         super.onCreate(savedInstanceState)
         binding = setContentView(this, activity_main)
     }
